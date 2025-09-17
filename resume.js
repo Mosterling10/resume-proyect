531c5170f7cf2bdcc52d6e47d50b75ebcae2a585
Micaella Osterling <Mosterling_10@oru.edu>
2025-09-17 09:17:24 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,14 @@
+/ / Define Variables
+let myName = "Micaella";
+let myEmail = "Mosterling_10@oru.edu";
+let title = "Student";
+let education = "Oral Roberts University";
+let experience = "Student and Assistant";
+/ / let skills = "something"; / / TransformStreamDefaultController
+
+/ / Connect Variables to the HTML (ligaments)
+document.getElementById("my-name").textContent = myName;
+document.getElementById("my-email").textContent = myEmail;
+document.getElementById("title").textContent = title;
+document.getElementById("experience").textContent = experience;
+document.getElementById("education").textContent = education;
